0d61432b6621cd71372c7369a01f06c583a975c6
Mohit Kotecha <kotechamohit14@gmail.com>
2024-10-18 19:35:43 +0530

    Swagger-UI

--- src/main/java/com/example/song/SongApplication.java
+++ src/main/java/com/example/song/SongApplication.java
@@ -3,11 +3,12 @@ package com.example.song;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SongApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SongApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SongApplication.class, args);
+    }
 
 }
